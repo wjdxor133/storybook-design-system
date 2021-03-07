9aeee29daec26f5dfe92b864e5425a52a2e70c7f
wjdxor133 <wjdxor133@naver.com>
2021-03-07 13:51:31 +0900

    Storybook typescript added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,28 @@
+const path = require("path");
+
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.(js|mdx|tsx)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-docs/preset",
+  ],
+
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [["react-app", { flow: false, typescript: true }]],
+          },
+        },
+        require.resolve("react-docgen-typescript-loader"),
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
 };
